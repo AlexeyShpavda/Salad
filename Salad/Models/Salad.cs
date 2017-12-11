fef66c9815706a5f16b81719db21c1b550be4089
Alexey Shpavda <alexeyshpavda@gmail.com>
2017-12-11 23:27:21 +0300

    Added salad and vegetable models

---
+++ Salad/Models/Salad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Salad.Interfaces;
+using System.IO;
+using Salad.Breakers;
+
+namespace Salad.Models
+{
+    class Salad : IReadFiles, IWriteToFiles
+    {
+
+        List<Vegetable> _salad = new List<Vegetable>();
+
+        internal List<Vegetable> _Salad { get => _salad; set => _salad = value; }
+
+        public void ReadingFile(string fileName)
+        {
+            string str;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while ((str = reader.ReadLine()) != null)
+                {
+                    string[] words = LineBreaker.ReturnWordArr(' ', str, 0);
+                    _Salad.Add(new Vegetable(words[0], words[1], words[2], words[3]));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = String.Empty;
+            foreach (var element in _Salad)
+            {
+                str += element.ToString() + "\r\n";
+            }
+            return str;
+        }
+
+        public void WriteToFiles(string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.Write(ToString());
+                writer.WriteLine("СaloriesInSalad: " + СountsСalories());
+            }
+        }
+
+        public int СountsСalories()
+        {
+            int _caloricity = 0;
+            foreach (Vegetable vegatable in _Salad)
+            {
+                _caloricity += vegatable.Caloricity;
+            }
+            return _caloricity;
+        }
+
+    }
+}
---
+++ Salad/Models/Vegetable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salad.Models
+{
+    class Vegetable : IComparable<Vegetable>
+    {
+        private string _name;
+        private short _weight;
+        private short _caloricity;
+        private double _price;
+
+        public string Name { get => _name; set => _name = value; }
+        public short Weight { get => _weight; set => _weight = value; }
+        public short Caloricity { get => _caloricity; set => _caloricity = value; }
+        public double Price { get => _price; set => _price = value; }
+
+        public Vegetable(string name, string weight, string caloricity, string price)
+        {
+            Name = name;
+            Weight = Int16.Parse(weight);
+            Caloricity = Int16.Parse(caloricity);
+            Price = Double.Parse(price);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0,-10}|Weight: {1,-4}|Caloricity: {2,-3}|Price: {3,-4}",
+                Name, Weight, Caloricity, Price);
+        }
+
+        public int CompareTo(Vegetable obj)
+        {
+            if (Caloricity > obj.Caloricity)
+                return 1;
+            if (Caloricity < obj.Caloricity)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
